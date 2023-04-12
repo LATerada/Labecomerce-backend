c2e684f1f475114e551bbbe111b6f4f11daeb942
LATerada <larissaakiterada@gmail.com>
2023-04-12 15:40:57 -0300

    GET all products updated

--- src/endpoints/getAllProducts.ts
+++ src/endpoints/getAllProducts.ts
@@ -3,10 +3,13 @@ import { db } from "../database/knex";
 
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
-    // const products = await db.raw(`
-    // SELECT * FROM products;
-    // `);
-    const products = await db("products");
+    const products = await db("products").select(
+      "id",
+      "name",
+      "price",
+      "description",
+      "image_url as imageUrl"
+    );
     res.status(200).send(products);
   } catch (error) {
     console.log(error);
