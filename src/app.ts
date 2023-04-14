9566c82392dba01ea1cff846f37c2db90c172965
LATerada <larissaakiterada@gmail.com>
2023-04-14 14:56:54 -0300

    deade code cleared

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { test } from "./endpoints/test";
-import { getAllUsers } from "./endpoints/getAllUsers";
-import { getAllProducts } from "./endpoints/getAllProducts";
 import { createUsers } from "./endpoints/createUsers";
-import { searchProductsByName } from "./endpoints/searchProductByName";
 import { createProduct } from "./endpoints/createProduct";
 import { createPurchase } from "./endpoints/createPurchase";
-import { getProductById } from "./endpoints/getProductById";
+import { getAllUsers } from "./endpoints/getAllUsers";
+import { getAllProducts } from "./endpoints/getAllProducts";
+import { getPurchaseById } from "./endpoints/getPurchaseById";
+// import { searchProductsByName } from "./endpoints/searchProductByName";
+// import { getProductById } from "./endpoints/getProductById";
 // import { getUserPurchaseByUserId } from "./endpoints/getUserPurchaseByUserId";
 // import { deleteUserById } from "./endpoints/deleteUserById";
-import { deleteProductById } from "./endpoints/deleteProductById";
+// import { deleteProductById } from "./endpoints/deleteProductById";
 // import { editUserById } from "./endpoints/editUserById";
 import { editProductById } from "./endpoints/editProductById";
 import { deletePurchaseById } from "./endpoints/deletePurchaseById";
-import { getPurchaseById } from "./endpoints/getPurchaseById";
+
 // import { getAllPurchases } from "./endpoints/getAllPurhcases";
 
 const app = express();
@@ -29,18 +30,6 @@ app.listen(3003, () => {
 //test
 app.get("/ping", test);
 
-//getAllUsers
-app.get("/users", getAllUsers);
-
-// getAllProducts
-app.get("/products", getAllProducts);
-
-// //getAllPurchases
-// app.get("/purchases", getAllPurchases);
-
-// searchProductByName
-app.get("/products/search", searchProductsByName);
-
 // createUser
 app.post("/users", createUsers);
 
@@ -50,20 +39,14 @@ app.post("/products", createProduct);
 // createPurchase
 app.post("/purchases", createPurchase);
 
-// getProductById
-app.get("/products/:id", getProductById);
+//getAllUsers
+app.get("/users", getAllUsers);
 
-// // getUserPurchaseByUserId
-// app.get("/purchases/:id", getUserPurchaseByUserId);
+// getAllProducts
+app.get("/products", getAllProducts);
 
-// // deleteUserById
-// app.delete("/users/:id", deleteUserById);
-
-// deleteProductById
-app.delete("/products/:id", deleteProductById);
-
-// // editUserById
-// app.put("/users/:id", editUserById);
+// getPurchaseById
+app.get("/purchases/:id", getPurchaseById);
 
 // editProductById
 app.put("/products/:id", editProductById);
@@ -71,5 +54,23 @@ app.put("/products/:id", editProductById);
 // // deletePurchaseById
 app.delete("/purchases/:id", deletePurchaseById);
 
-// getPurchaseById
-app.get("/purchases/:id", getPurchaseById);
+// //getAllPurchases
+// app.get("/purchases", getAllPurchases);
+
+// // searchProductByName
+// app.get("/products/search", searchProductsByName);
+
+// // getProductById
+// app.get("/products/:id", getProductById);
+
+// // getUserPurchaseByUserId
+// app.get("/purchases/:id", getUserPurchaseByUserId);
+
+// // deleteUserById
+// app.delete("/users/:id", deleteUserById);
+
+// // deleteProductById
+// app.delete("/products/:id", deleteProductById);
+
+// // editUserById
+// app.put("/users/:id", editUserById);
--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,8 +1,12 @@
 -- Active: 1680617537489@@127.0.0.1@3306
 
--- |    USERS   |
-
 DROP TABLE users;
+
+DROP TABLE products;
+
+DROP TABLE purchases;
+
+DROP TABLE purchases_products;
 
 CREATE TABLE
     users (
@@ -11,6 +15,34 @@ CREATE TABLE
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
         created_at TEXT DEFAULT (DATETIME())
+    );
+
+CREATE TABLE
+    products (
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        description TEXT NOT NULL,
+        image_url TEXT NOT NULL
+    );
+
+CREATE TABLE
+    purchases (
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        buyer TEXT NOT NULL,
+        total_price REAL NOT NULL,
+        created_at DATE DEFAULT (DATETIME()),
+        paid INTEGER DEFAULT (0),
+        FOREIGN KEY (buyer) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+CREATE TABLE
+    purchases_products (
+        purchase_id TEXT NOT NULL,
+        product_id TEXT NOT NULL,
+        quantity INTEGER NOT NULL,
+        FOREIGN KEY (purchase_id) REFERENCES purchases(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
 INSERT INTO
@@ -35,27 +67,6 @@ VALUES (
         "User3",
         "user3@email.com",
         12303
-    );
-
-DELETE from users WHERE "id"='u002';
-
-UPDATE users SET password = 12300 WHERE id = 'u000';
-
-SELECT * FROM users;
-
-SELECT * FROM users ORDER BY email ASC;
-
--- |    PRODUCTS    |
-
-DROP TABLE products;
-
-CREATE TABLE
-    products (
-        id TEXT PRIMARY KEY UNIQUE NOT NULL,
-        name TEXT NOT NULL,
-        price REAL NOT NULL,
-        description TEXT NOT NULL,
-        image_url TEXT NOT NULL
     );
 
 INSERT INTO products
@@ -91,72 +102,17 @@ VALUES (
         "https://www.new-york-city-travel-tips.com/wordpress/wp-content/uploads/2014/11/Museum-of-the-Moving-Image-MPVNY-BPVNY-NYCTT-2.jpg"
     );
 
-DELETE FROM products WHERE id = "p005";
-
-SELECT * FROM products;
-
-SELECT * FROM products WHERE name = 'monitor';
-
-SELECT * FROM products WHERE id = "p002";
-
-SELECT * FROM products ORDER BY price ASC LIMIT 20;
-
-SELECT *
-FROM products
-WHERE price > 10 AND price < 30
-ORDER BY price ASC;
-
--- |    PURCHASES   |
-
-DROP TABLE purchases;
-
-CREATE TABLE
-    purchases (
-        id TEXT PRIMARY KEY UNIQUE NOT NULL,
-        buyer TEXT NOT NULL,
-        total_price REAL NOT NULL,
-        created_at DATE DEFAULT (DATETIME()),
-        paid INTEGER DEFAULT (0),
-        FOREIGN KEY (buyer) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
-
 INSERT INTO
     purchases (id, buyer, total_price)
 VALUES ("pr000", "u000", 120), ("pr001", "u000", 50), ("pr002", "u001", 500), ("pr003", "u002", 50), ("pr004", "u002", 110), ("pr005", "u003", 90);
 
-DELETE FROM purchases WHERE id = "pr003";
-
-SELECT * FROM purchases;
-
-SELECT *
-FROM purchases
-    INNER JOIN users ON purchases.buyer = users.id
-WHERE users.id = "u000";
-
--- |  PURCHASE PRODUCTS   |
-
-DROP TABLE purchases_products;
-
-CREATE TABLE
-    purchases_products (
-        purchase_id TEXT NOT NULL,
-        product_id TEXT NOT NULL,
-        quantity INTEGER NOT NULL,
-        FOREIGN KEY (purchase_id) REFERENCES purchases(id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
-
 INSERT INTO purchases_products
 VALUES ("pr000", "p002", 1), ("pr000", "p001", 2), ("pr001", "p000", 2), ("pr002", "p003", 10), ("pr002", "p000", 10), ("pr002", "p002", 10);
 
-SELECT
-    purchases.id as purchaseId,
-    purchases.buyer as buyerId,
-    products.id AS productId,
-    products.name AS productName,
-    purchases_products.quantity AS productQuantity,
-    products.price AS productPrice,
-    purchases.total_price
-FROM purchases
-    INNER JOIN purchases_products ON purchases.id = purchases_products.purchase_id
-    INNER JOIN products ON purchases_products.product_id = products.id;
+SELECT * FROM users;
+
+SELECT * FROM products;
+
+SELECT * FROM purchases;
+
+SELECT * FROM purchases_products;
