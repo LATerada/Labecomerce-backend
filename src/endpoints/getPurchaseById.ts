474794320be77aa85bf2a5bc658726e59b02c77f
LATerada <larissaakiterada@gmail.com>
2023-04-14 13:30:37 -0300

    GET purchaseById updated

--- src/endpoints/getPurchaseById.ts
+++ src/endpoints/getPurchaseById.ts
@@ -1,19 +1,55 @@
 import { Request, Response } from "express";
 import { db } from "../database/knex";
-import { TPurchase } from "../types";
+import { TProduct, TPurchase, TPurchaseProduct } from "../types";
 
 export const getPurchaseById = async (req: Request, res: Response) => {
   try {
-    const id = req.params.id;
-    const [purchase]: TPurchase[] | undefined[] = await db("purchases").where({
-      id,
-    });
+    const purchaseId = req.params.id;
 
+    const [purchase]: TPurchase[] | undefined[] = await db("purchases").where({
+      id: purchaseId,
+    });
     if (!purchase) {
       res.status(404);
       throw new Error("Compra não existe");
     }
-    res.status(200).send(purchase);
+
+    const [result] = await db("purchases")
+      .select(
+        "purchases.id as purchaseId",
+        "users.id as buyerId",
+        "users.name as userName",
+        "users.email as buyerEmail",
+        "purchases.total_price as totalPrice",
+        "purchases.created_at as createdAt",
+        "purchases.paid"
+      )
+      .where({ purchaseId })
+      .innerJoin("users", "users.id", "=", "purchases.buyer");
+
+    const purchaseProducts: TPurchaseProduct[] | undefined[] = await db(
+      "purchases_products"
+    ).where({ purchase_id: purchaseId });
+
+    let products = [];
+
+    for (let product of purchaseProducts) {
+      const [productFromPurchase]: TProduct[] = await db("products").where({
+        id: product.product_id,
+      });
+      const newProduct = {
+        ...productFromPurchase,
+        quantity: product.quantity,
+      };
+      products.push(newProduct);
+    }
+
+    const newResult = {
+      ...result,
+      products,
+    };
+
+    res.status(200).send(newResult);
   } catch (error) {
     console.log(error);
 
